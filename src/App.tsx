6d23bbb365fd40894d1d3eda0b79ab04a7767b32
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-03-18 21:11:21 +0900

    enviromnent

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center p-2 m-2 font-bold text-lg'>Hello Vite & React.ts</h1>
+    </>
+  )
+}
+
+export default App
